983a19b1199228870a2f42b8020120385b1a1609
satoshi <dev.satoshi.1016@gmail.com>
2023-06-17 04:22:08 +0900

    add: カスタムユーザー、Google認証を作成

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
 
-# Create your views here.
+class GoogleLoginView(SocialLoginView):
+  adapter_class = GoogleOAuth2Adapter
+  client_class = OAuth2Client
